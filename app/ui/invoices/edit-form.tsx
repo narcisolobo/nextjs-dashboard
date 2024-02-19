cf732e510fac147d2fe85e0469eec718c06f26cf
narcisolobo <narcisolobo@gmail.com>
2024-02-19 11:48:13 -0800

    up to and including chapter 12

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,16 +9,18 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
-export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
+interface EditInvoiceFormProps {
   invoice: InvoiceForm;
   customers: CustomerField[];
-}) {
+}
+
+function EditInvoiceForm({ invoice, customers }: EditInvoiceFormProps) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -121,3 +123,5 @@ export default function EditInvoiceForm({
     </form>
   );
 }
+
+export default EditInvoiceForm;
